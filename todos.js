d76062091b18426200968025d8ab9a9adb0af6ec
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-04 13:51:26 +0530

    adding new list

--- todos.js
+++ todos.js
@@ -7,9 +7,13 @@ while (input !== "quit" && input !== 'q') {
     if (input === "list") {
         console.log("**********")
         for (let i = 0; i < todos.length; i++) {
-            console.log(`#${i}: ${todos[i]}`);
+            console.log(`${i}: ${todos[i]}`);
         }
         console.log("**********")
+    } else if (input === "new") {
+        const newTodo = prompt("Ok, what is the new todo?");
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
     }
     input = prompt("What would you like to do")
 }
